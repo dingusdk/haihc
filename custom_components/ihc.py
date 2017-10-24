d4bce679f26b90f36f79beef07ab22d883ac1a8d
Jens <jensoen@gmail.com>
2017-10-24 23:32:00 +0200

    Automatically import of ihcsdk from PyPi

--- custom_components/ihc.py
+++ custom_components/ihc.py
@@ -2,16 +2,14 @@ import logging
 import threading
 import time
 from homeassistant.helpers.discovery import load_platform
-from ihcsdk.ihcclient import IHCSoapClient
-from ihcsdk.ihccontroller import IHCController
 
 DOMAIN = 'ihc'
-#REQUIREMENTS = ['ihcsdk==1.0.0']
+REQUIREMENTS = [ 'ihcsdk==1.0.2']
 
 _LOGGER = logging.getLogger(__name__)
 
 def setup(hass, config):
-
+    from ihcsdk.ihccontroller import IHCController
     url = config[DOMAIN].get('url')
     username = config[DOMAIN].get('username')
     password = config[DOMAIN].get('password')
