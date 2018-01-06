c1afff8e673691bb955f983a5cb2bfe293db04e8
Jens <jensoen@gmail.com>
2018-01-06 15:38:09 +0100

    PR review changes

--- custom_components/sensor/ihc.py
+++ custom_components/sensor/ihc.py
@@ -1,101 +1,69 @@
+"""IHC sensor platform.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/sensor.ihc/
 """
-IHC sensor platform.
-"""
-# pylint: disable=too-many-arguments, too-many-instance-attributes, bare-except, unused-argument, missing-docstring
-import logging
+from xml.etree.ElementTree import Element
 import voluptuous as vol
+
+from ..ihc.const import CONF_AUTOSETUP
+from ..ihc import validate_name, IHC_DATA
+from ..ihc.ihcdevice import IHCDevice
 from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import (
+    CONF_ID, CONF_NAME, CONF_TYPE, CONF_UNIT_OF_MEASUREMENT, CONF_SENSORS)
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
-from homeassistant.const import (CONF_ID, CONF_NAME, CONF_TYPE,
-    CONF_UNIT_OF_MEASUREMENT, CONF_SENSORS)
-
-from ..ihc.const import *
-from ..ihc import get_ihc_platform
-from ..ihc.ihcdevice import IHCDevice
 
 DEPENDENCIES = ['ihc']
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_AUTOSETUP, default='False'): cv.boolean,
-    vol.Optional(CONF_SENSORS) :
-        [{
-            vol.Required(CONF_ID): cv.positive_int,
-            vol.Optional(CONF_NAME): cv.string,
-            vol.Optional(CONF_TYPE): cv.string,
-            vol.Optional(CONF_UNIT_OF_MEASUREMENT): cv.string
-        }]
+    vol.Optional(CONF_SENSORS, default=[]):
+        vol.All(cv.ensure_list, [
+            vol.All({
+                vol.Required(CONF_ID): cv.positive_int,
+                vol.Optional(CONF_NAME): cv.string,
+                vol.Optional(CONF_TYPE, default='Temperature'): cv.string,
+                vol.Optional(CONF_UNIT_OF_MEASUREMENT, default='°C'): cv.string
+            }, validate_name)
+        ])
 })
 
-PRODUCTAUTOSETUP = [
-    # Temperature sensor
-    {'xpath': './/product_dataline[@product_identifier="_0x2124"]',
-     'node': 'resource_temperature',
-     'sensortype': "Temperature",
-     'unit_of_measurement': '°C'},
-    # Humidity/temperature
-    {'xpath': './/product_dataline[@product_identifier="_0x2135"]',
-     'node': 'resource_humidity_level',
-     'sensortype': "Humidity",
-     'unit_of_measurement': '%'},
-    # Humidity/temperature
-    {'xpath': './/product_dataline[@product_identifier="_0x2135"]',
-     'node': 'resource_temperature',
-     'sensortype': "Temperature",
-     'unit_of_measurement': '°C'},
-    # Lux/temperature
-    {'xpath': './/product_dataline[@product_identifier="_0x2136"]',
-     'node': 'resource_light',
-     'sensortype': "Light",
-     'unit_of_measurement': 'Lux'},
-    # Lux/temperature
-    {'xpath': './/product_dataline[@product_identifier="_0x2136"]',
-     'node': 'resource_temperature',
-     'sensortype': "Temperature",
-     'unit_of_measurement': '°C'},
-]
-
-_LOGGER = logging.getLogger(__name__)
-
-_IHCSENSORS = {}
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Setup the ihc sensor platform"""
-    ihcplatform = get_ihc_platform(hass)
+    """Setup the ihc sensor platform."""
+    ihc = hass.data[IHC_DATA]
     devices = []
-    if config.get('autosetup'):
-        auto_setup(ihcplatform, devices)
+    if config.get(CONF_AUTOSETUP):
+        def setup_product(ihc_id, name, product, product_cfg):
+            """Product setup callback."""
+            sensor = IHCSensor(ihc, name, ihc_id,
+                               product_cfg[CONF_TYPE],
+                               product_cfg[CONF_UNIT_OF_MEASUREMENT],
+                               product)
+            devices.append(sensor)
+        ihc.auto_setup('sensor', setup_product)
 
     sensors = config.get(CONF_SENSORS)
-    if sensors != None:
-        _LOGGER.info("Adding IHC Sensor")
-        for sensor in sensors:
-            ihcid = sensor[CONF_ID]
-            name = sensor[CONF_NAME] if CONF_NAME in sensor else "ihc_" + str(ihcid)
-            sensortype = sensor[CONF_TYPE] if CONF_TYPE in sensor else "Temperature"
-            unit = sensor[CONF_UNIT_OF_MEASUREMENT] if CONF_UNIT_OF_MEASUREMENT in sensor else "°C"
-            add_sensor(devices, ihcplatform.ihc, int(ihcid), name, sensortype, unit, True)
+    for sensor_cfg in sensors:
+        ihc_id = sensor_cfg[CONF_ID]
+        name = sensor_cfg[CONF_NAME]
+        sensor_type = sensor_cfg[CONF_TYPE]
+        unit = sensor_cfg[CONF_UNIT_OF_MEASUREMENT]
+        sensor = IHCSensor(ihc, name, ihc_id, sensor_type, unit)
+        devices.append(sensor)
 
     add_devices(devices)
-    # Start notification after devices has been added
-    for sensor in devices:
-        sensor.ihc.add_notify_event(sensor.get_ihcid(), sensor.on_ihc_change, True)
-
-def auto_setup(ihcplatform, devices):
-    """Setup ihc sensors from the ihc project file."""
-    _LOGGER.info("Auto setup for IHC sensor")
-    def setup_product(ihcid, name, product, productcfg):
-        add_sensor(devices, ihcplatform.ihc, ihcid, name,
-                   productcfg['sensortype'], productcfg['unit_of_measurement'],
-                   False, product.attrib['name'], product.attrib['note'],
-                   product.attrib['position'])
-    ihcplatform.autosetup(PRODUCTAUTOSETUP, setup_product)
 
 
 class IHCSensor(IHCDevice, Entity):
     """Implementation of the IHC sensor."""
-    def __init__(self, ihccontroller, name, ihcid, sensortype, unit, ihcname, ihcnote, ihcposition):
-        IHCDevice.__init__(self, ihccontroller, name, ihcid, ihcname, ihcnote, ihcposition)
+
+    def __init__(self, ihc, name, ihc_id, sensortype, unit,
+                 product: Element = None):
+        """Initialize the IHC sensor."""
+        super().__init__(ihc, name, ihc_id, product)
         self._state = None
         self._icon = None
         self._assumed = False
@@ -112,37 +80,11 @@ class IHCSensor(IHCDevice, Entity):
         """Return the unit of measurement of this entity, if any."""
         return self._unit_of_measurement
 
-    def update(self):
-        pass
-
     def set_unit(self, unit):
-        """Set unit of measusement"""
+        """Set unit of measusement."""
         self._unit_of_measurement = unit
 
-    def on_ihc_change(self, ihcid, value):
+    def on_ihc_change(self, ihc_id, value):
         """Callback when ihc resource changes."""
-        try:
-            self._state = value
-            self.schedule_update_ha_state()
-        except:
-            pass
-
-def add_sensor(devices, ihccontroller, ihcid: int, name: str, sensortype: str,
-               unit: str, overwrite: bool = False,
-               ihcname: str = "", ihcnote: str = "", ihcposition: str = "") -> IHCSensor:
-    """Add a new ihc sensor"""
-    if ihcid in _IHCSENSORS:
-        sensor = _IHCSENSORS[ihcid]
-        if overwrite:
-            sensor.set_name(name)
-            sensor.type = sensortype
-            sensor.set_unit(unit)
-            _LOGGER.info("IHC sensor set name: " + name + " " + str(ihcid))
-    else:
-        sensor = IHCSensor(ihccontroller, name, ihcid, sensortype, unit,
-                           ihcname, ihcnote, ihcposition)
-        sensor.type = sensortype
-        _IHCSENSORS[ihcid] = sensor
-        devices.append(sensor)
-        _LOGGER.info("IHC sensor added: " + name + " " + str(ihcid))
-    return sensor
+        self._state = value
+        self.schedule_update_ha_state()
--- custom_components/switch/ihc.py
+++ custom_components/switch/ihc.py
@@ -1,83 +1,66 @@
+"""IHC switch platform.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/switch.ihc/
 """
-IHC switch platform that implements a switch.
-"""
-# pylint: disable=too-many-arguments, too-many-instance-attributes, bare-except, unused-argument, missing-docstring
-import logging
+from xml.etree.ElementTree import Element
 import voluptuous as vol
-import homeassistant.helpers.config_validation as cv
+
+from ..ihc.const import CONF_AUTOSETUP
+from ..ihc import validate_name, IHC_DATA
+from ..ihc.ihcdevice import IHCDevice
 from homeassistant.components.switch import (SwitchDevice, PLATFORM_SCHEMA)
 from homeassistant.const import CONF_ID, CONF_NAME, CONF_SWITCHES
-
-from ..ihc.const import *
-from ..ihc import get_ihc_platform
-from ..ihc.ihcdevice import IHCDevice
+import homeassistant.helpers.config_validation as cv
 
 DEPENDENCIES = ['ihc']
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_AUTOSETUP, default='False'): cv.boolean,
-    vol.Optional(CONF_SWITCHES) :
-        [{
-            vol.Required(CONF_ID): cv.positive_int,
-            vol.Optional(CONF_NAME): cv.string,
-        }]
+    vol.Optional(CONF_SWITCHES, default=[]):
+        vol.All(cv.ensure_list, [
+            vol.All({
+                vol.Required(CONF_ID): cv.positive_int,
+                vol.Optional(CONF_NAME): cv.string,
+            }, validate_name)
+        ])
 })
 
-PRODUCTAUTOSETUP = [
-    # Wireless Plug outlet
-    {'xpath': './/product_airlink[@product_identifier="_0x4201"]',
-     'node': 'airlink_relay'},
-    # Dataline universal relay
-    {'xpath': './/product_airlink[@product_identifier="_0x4203"]',
-     'node': 'airlink_relay'},
-    # Dataline plug outlet
-    {'xpath': './/product_dataline[@product_identifier="_0x2201"]',
-     'node': 'dataline_output'},
-    ]
 
-
-_LOGGER = logging.getLogger(__name__)
-
-_IHCSWITCHES = {}
-
-def setup_platform(hass, config, add_devices_callback, discovery_info=None):
+def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the ihc switch platform."""
-    ihcplatform = get_ihc_platform(hass)
+    ihc = hass.data[IHC_DATA]
     devices = []
-    if config.get('autosetup'):
-        auto_setup(ihcplatform, devices)
+    if config.get(CONF_AUTOSETUP):
+        def setup_product(ihc_id, name, product, product_cfg):
+            """Product setup callback."""
+            sensor = IHCSwitch(ihc, name, ihc_id, product)
+            devices.append(sensor)
+        ihc.auto_setup('switch', setup_product)
 
     switches = config.get(CONF_SWITCHES)
-    if switches:
-        _LOGGER.info("Adding IHC Switches")
-        for switch in switches:
-            ihcid = switch[CONF_ID]
-            name = switch[CONF_NAME] if CONF_NAME in switch else "ihc_" + str(ihcid)
-            add_switch(devices, ihcplatform.ihc, int(ihcid), name, True)
+    for switch in switches:
+        ihc_id = switch[CONF_ID]
+        name = switch[CONF_NAME]
+        sensor = IHCSwitch(ihc, name, ihc_id)
+        devices.append(sensor)
 
-    add_devices_callback(devices)
-    # Start notification after device har been added
-    for device in devices:
-        device.ihc.add_notify_event(device.get_ihcid(), device.on_ihc_change, True)
+    add_devices(devices)
 
-def auto_setup(ihcplatform, devices):
-    """Auto setup switched from the ihc project file."""
-    _LOGGER.info("Auto setup for IHC switch")
-    def setup_product(ihcid, name, product, productcfg):
-        add_switch_from_node(devices, ihcplatform.ihc, ihcid, name, product)
-    ihcplatform.autosetup(PRODUCTAUTOSETUP, setup_product)
 
 class IHCSwitch(IHCDevice, SwitchDevice):
     """IHC Switch."""
-    def __init__(self, ihccontroller, name: str, ihcid: int,
-                 ihcname: str, ihcnote: str, ihcposition: str):
-        IHCDevice.__init__(self, ihccontroller, name, ihcid, ihcname, ihcnote, ihcposition)
+
+    def __init__(self, ihc, name: str, ihc_id: int, product: Element = None):
+        """Initialize the IHC switch."""
+        super().__init__(ihc, name, ihc_id, product)
         self._state = False
         self._icon = None
         self._assumed = False
 
     @property
     def should_poll(self):
+        """We do not need to poll."""
         return False
 
     @property
@@ -108,42 +91,16 @@ class IHCSwitch(IHCDevice, SwitchDevice):
     def turn_on(self, **kwargs):
         """Turn the switch on."""
         self._state = True
-        self.ihc.set_runtime_value_bool(self._ihcid, True)
+        self.ihc.ihc_controller.set_runtime_value_bool(self._ihc_id, True)
         self.schedule_update_ha_state()
 
     def turn_off(self, **kwargs):
         """Turn the device off."""
         self._state = False
-        self.ihc.set_runtime_value_bool(self._ihcid, False)
+        self.ihc.ihc_controller.set_runtime_value_bool(self._ihc_id, False)
         self.schedule_update_ha_state()
 
-    def on_ihc_change(self, ihcid, value):
+    def on_ihc_change(self, ihc_id, value):
         """Callback when the ihc resource changes."""
-        try:
-            self._state = value
-            self.schedule_update_ha_state()
-        except:
-            pass
-
-
-def add_switch_from_node(devices, ihccontroller, ihcid: int, name: str, product) -> IHCSwitch:
-    """Add a IHC switch form the a product in the project."""
-    ihcname = product.attrib['name']
-    ihcnote = product.attrib['note']
-    ihcposition = product.attrib['position']
-    return add_switch(devices, ihccontroller, ihcid, name, False, ihcname, ihcnote, ihcposition)
-
-def add_switch(devices, ihccontroller, ihcid: int, name: str, overwrite: bool = False,
-               ihcname: str = "", ihcnote: str = "", ihcposition: str = "") -> IHCSwitch:
-    """Add a new ihc switch"""
-    if ihcid in _IHCSWITCHES:
-        switch = _IHCSWITCHES[ihcid]
-        if overwrite:
-            switch.set_name(name)
-            _LOGGER.info("IHC switch set name: " + name + " " + str(ihcid))
-    else:
-        switch = IHCSwitch(ihccontroller, name, ihcid, ihcname, ihcnote, ihcposition)
-        _IHCSWITCHES[ihcid] = switch
-        devices.append(switch)
-        _LOGGER.info("IHC switch added: " + name + " " + str(ihcid))
-    return switch
+        self._state = value
+        self.schedule_update_ha_state()
